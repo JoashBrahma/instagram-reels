b8f28793aba8f22cefb64e97dda76f14c80a190d
Joash Brahma <joashbrahma@zohomail.com>
2025-11-25 16:17:13 +0530

    chore: initial commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+import reels from "./reels.js";
+
+const reelsContainer = document.querySelector(".reels-container");
+
+reelsContainer.innerHTML =
+  reels.reduce((acc, reel, idx) => {
+    return acc += `
+      <div class="reel">
+        <div class="reel__content">
+          <video src="${reel.video}" autoplay muted loop></video>
+        </div>
+        <div class="reel__bottom">
+          <div class="reel__profile">
+            <div class="reel__profile-pic">
+              <img src="${reel.profilePic}" alt="${reel.username}'s profile picture">
+            </div>
+            <p class="reel__username">${reel.username}</p>
+            <button type="button" class="reel__btn reel__btn-follow" data-reel-id="${idx}">
+              ${reel.isFollowed ? "Following" : "Follow"}
+            </button>
+          </div>
+          <p class="reel__caption">${reel.caption}</p>
+        </div>
+        <div class="reel__right">
+          <div class="reel__action">
+            <button type="button" class="reel__btn reel__btn-like ${reel.isLiked ? 'liked' : "" }" data-reel-id="${idx}">
+              <i class="ri-heart-${reel.isLiked ? 'fill' : "line" }"></i>
+            </button>
+            <p class="reel__total-likes">${reel.likes}</p>
+          </div>
+          <div class="reel__action">
+            <button type="button" class="reel__btn reel__btn-comment">
+              <i class="ri-chat-3-line"></i>
+            </button>
+            <p class="reel__total-comments">${reel.comments}</p>
+          </div>
+          <div class="reel__action">
+            <button type="button" class="reel__btn reel__btn-remix">
+              <i class="ri-remix-line"></i>
+            </button>
+          </div>
+          <div class="reel__action">
+            <button type="button" class="reel__btn reel__btn-share">
+              <i class="ri-share-forward-line"></i>
+            </button>
+            <p class="reel__total-shares">${reel.shares}</p>
+          </div>
+          <div class="reel__action">
+            <button type="button" class="reel__btn reel__btn-more">
+              <i class="ri-more-2-line"></i>
+            </button>
+          </div>
+          <div class="reel__action">
+            <button type="button" class="reel__btn reel__btn-music">
+              <i class="ri-music-2-line"></i>
+            </button>
+          </div>
+        </div>
+      </div>`;
+  }, "");
---
+++ js/reels.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    profilePic: "./assets/images/image1.jpg",
+    username: "maisahere",
+    isFollowed: true,
+    video: "./assets/videos/video1.mp4",
+    caption: "Chasing moments, one clip at a time.",
+    isLiked: false,
+    likes: 12,
+    comments: 12,
+    shares: 12
+  },
+  {
+    profilePic: "./assets/images/image2.jpg",
+    username: "bidinw.swi",
+    isFollowed: false,
+    video: "./assets/videos/video2.mp4",
+    caption: "Another day, another story.",
+    isLiked: true,
+    likes: 20,
+    comments: 8,
+    shares: 4
+  },
+  {
+    profilePic: "./assets/images/image3.jpg",
+    username: "thewanderer",
+    isFollowed: true,
+    video: "./assets/videos/video3.mp4",
+    caption: "Life looks better in motion.",
+    isLiked: false,
+    likes: 35,
+    comments: 16,
+    shares: 9
+  },
+  {
+    profilePic: "./assets/images/image4.jpg",
+    username: "creative.soul",
+    isFollowed: false,
+    video: "./assets/videos/video4.mp4",
+    caption: "Small clips. Big vibes.",
+    isLiked: true,
+    likes: 5,
+    comments: 1,
+    shares: 0
+  }
+];
